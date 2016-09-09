843bbed5313770753415020f04f2411d8aeb9329
Wellington Barreto <zeindelf@hotmail.com>
2016-09-08 23:18:46 -0300

    New routes display

--- src/js/gmaps/getUserLocation.js
+++ src/js/gmaps/getUserLocation.js
@@ -1,7 +1,5 @@
 
 import { initMap } from './initMap.js'
-import { getAddress } from './getAddress'
-import { nearbyLocation } from './nearbyLocation'
 
 export function getUserLocation () {
 
@@ -11,5 +9,60 @@ export function getUserLocation () {
     $.ajax({
         url: `${geoLocationUrl}${apiKeyGeoLocation}`,
         type: "post"
-    }).done(initMap, getAddress, nearbyLocation)
+    // }).done(initMap)
+    }).done( (data) => {
+        iMap()
+    })
  }
+
+function iMap() {
+    var markerArray = [];
+    var directionsService = new google.maps.DirectionsService;
+    var map = new google.maps.Map(document.getElementById('map'), {
+        zoom: 13,
+        center: {lat: 40.771, lng: -73.974}
+    });
+
+    var directionsDisplay = new google.maps.DirectionsRenderer({map: map});
+    var stepDisplay = new google.maps.InfoWindow;
+
+    // Mostra a rota inicial e final
+    calculateAndDisplayRoute(directionsDisplay, directionsService, markerArray, stepDisplay, map);
+
+    var onChangeHandler = function() {
+        calculateAndDisplayRoute(directionsDisplay, directionsService, markerArray, stepDisplay, map);
+    };
+
+    document.getElementById('start').addEventListener('change', onChangeHandler);
+    document.getElementById('end').addEventListener('change', onChangeHandler);
+    document.getElementById('mode').addEventListener('change', onChangeHandler);
+}
+
+function calculateAndDisplayRoute(directionsDisplay, directionsService, markerArray, stepDisplay, map) {
+
+    var selectedMode = document.getElementById('mode').value;
+
+    // Remove todas as marcações
+    for (var i = 0; i < markerArray.length; i++) {
+        markerArray[i].setMap(null);
+    }
+
+    directionsService.route({
+        origin: document.getElementById('start').value,
+        destination: document.getElementById('end').value,
+        travelMode: google.maps.TravelMode[selectedMode]
+    }, function(response, status) {
+        if (status === google.maps.DirectionsStatus.OK) {
+            directionsDisplay.setDirections(response);
+        } else {
+            window.alert('Directions request failed due to ' + status);
+        }
+    });
+}
+
+function attachInstructionText(stepDisplay, marker, text, map) {
+    google.maps.event.addListener(marker, 'click', function() {
+        stepDisplay.setContent(text)
+        stepDisplay.open(map, marker)
+    });
+}
